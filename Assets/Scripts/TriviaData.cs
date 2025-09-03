cba45c34efe13294b2637322a601c5111e3ae09f
Francisca Riffo <fran.riffo.astete@gmail.com>
2025-09-02 22:03:59 -0400

    [WIP] Setting up trivia

---
+++ Assets/Scripts/TriviaData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TriviaData : ScriptableObject
+{
+    public GameObject prefab;
+    public float preferredRotation;
+    public float preferredScale;
+    public TriviaQuestion[] questions;
+}
+
+/// <summary>
+/// The answer expects a child go name from the prefab in the trivia data
+/// </summary>
+public struct TriviaQuestion {
+    public string question;
+    public string answer;
+}
